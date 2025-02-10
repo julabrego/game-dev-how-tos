c2960def79bc4fb850e9f163ddf0b787f330cf1f
Julián Abrego <abrego.jul@gmail.com>
2025-02-10 20:59:59 -0300

    Adds acceleration movement to 3d eight directions

--- Godot/3d-eight-directions-movement/scripts/player/01-basic.gd
+++ Godot/3d-eight-directions-movement/scripts/player/01-basic.gd
@@ -5,12 +5,15 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+# This auxiliar variable will progressivelly calculate the CharacterBody3D 
+# velocity
 var target_velocity = Vector3.ZERO
-
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 
+	# If some direction input is pressed, its corresponding direction axis 
+	# will be 1 or -1
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -20,6 +23,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 
+	# The speed has to be normalized so if the player moves in two directions
+	# at the same time, the speed wont be faster than desired
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
